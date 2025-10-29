75a7a8fb9aad7cda958eb458b6635c9fba218fcd
analysis2025 <18917785124@163.com>
2025-10-29 16:03:19 +0800

    Update 222.py

--- 222.py
+++ 222.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost as xgb
 import pickle
 import joblib
 from io import BytesIO
@@ -241,5 +240,6 @@ except:
     ```
 
     """)
+
 
 
